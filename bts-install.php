87a8b543bd186ed576cae9e3a8d164e30eabf2cc
crestAT <info@a3s.at>
2014-11-09 09:40:17 +0200

    0.6.4
    verbose infos

--- bts-install.php
+++ bts-install.php
@@ -79,7 +79,7 @@ global $input_errors;
 global $savemsg;
 $install_dir = "./";
 if (isset($config['btsync']['rootfolder'])) { $install_dir = dirname($config['btsync']['rootfolder'])."/"; }
-$return_val = mwexec("fetch -o {$install_dir}master.zip 'http://sourceforge.net/projects/nas4freeextensionbts/files/BitTorrent%20Sync/bts-v064.zip/download'", true);
+$return_val = mwexec("fetch -vo {$install_dir}master.zip 'http://sourceforge.net/projects/nas4freeextensionbts/files/BitTorrent%20Sync/bts-v064.zip/download'", true);
 if ($return_val == 0) {
     $return_val = mwexec("tar -xf {$install_dir}master.zip -C {$install_dir} --exclude='.git*' --strip-components 1", true);
     if ($return_val == 0) {
